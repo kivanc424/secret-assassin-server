505ebd17971ab09f5edee3b92560c3c46ca203d3
Kivanc <kivancozmendev@hotmail.com>
2021-01-31 17:47:58 +0100

    save

---
+++ src/main/java/com/example/secretassassin/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.example.secretassassin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/rooms");
+        config.setApplicationDestinationPrefixes("/game");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/secret-assassin")
+                .setAllowedOrigins("http://localhost:3000").withSockJS();
+    }
+
+}
--- src/main/java/com/example/secretassassin/controller/CreateRoomController.java
+++ src/main/java/com/example/secretassassin/controller/CreateRoomController.java
@@ -2,11 +2,13 @@ package com.example.secretassassin.controller;
 
 import com.example.secretassassin.model.CreateRoom;
 import com.example.secretassassin.repository.CreateRoomRepository;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class CreateRoomController {
 
     public final CreateRoomRepository createRoomRepository;
@@ -16,15 +18,16 @@ public class CreateRoomController {
     }
 
     @PostMapping(value = "/create-room")
-    @CrossOrigin
+
     public CreateRoom createRoom(@RequestBody CreateRoom createRoom) {
         return createRoomRepository.insert(createRoom);
     }
 
 
     @GetMapping(value = "/get-all-rooms")
-    @CrossOrigin
+    @SendTo("/roomlists")
     public List<CreateRoom> getAllRooms() {
+
         return createRoomRepository.findAll();
     }
 }
